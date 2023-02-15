66b4e48e3bfc41ddbdc019752b44e66100ac2f2b
Diego Bonora <bonoradi@gmail.com>
2023-02-15 10:17:12 -0800

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet using putchar
+ * Return: int 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
