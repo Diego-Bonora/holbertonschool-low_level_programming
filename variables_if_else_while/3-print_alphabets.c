1b065de3a8b47b814def56e199f2340f3c892954
Diego Bonora <bonoradi@gmail.com>
2023-02-15 10:12:29 -0800

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet using putchar
+ * Return: int 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
