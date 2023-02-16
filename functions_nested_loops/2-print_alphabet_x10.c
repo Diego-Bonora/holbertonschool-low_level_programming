0914d0124f75751c69d59cea33d030f260b8fe1b
Diego Bonora <bonoradi@gmail.com>
2023-02-16 05:49:22 -0800

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int number;
 	int letter;
 
-	for (number = 0; number <= 10; number++)
+	for (number = 0; number <= 9; number++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
