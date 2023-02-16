ce4d6a7c1d5347e10885edebdd23adf52ebc4f56
Diego Bonora <bonoradi@gmail.com>
2023-02-16 10:09:12 -0800

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints time
+ * Return: nothing
+*/
+void jack_bauer(void)
+{
+	char hour;
+	char minute;
+
+	for (hour = "00"; hour <= "23"; hour++)
+	{
+	_putchar(hour);
+	}
+}
