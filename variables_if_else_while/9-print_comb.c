11228ca27799551ee3d878c15cd1f252243bd690
Diego Bonora <bonoradi@gmail.com>
2023-02-16 04:33:01 -0800

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers from 0 to 9 with ,
+ * Return: int 0
+*/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		putchar(number + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
