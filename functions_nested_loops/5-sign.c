14fa62b519eca7e543a053fd406e1cd2aa3f6432
Diego Bonora <bonoradi@gmail.com>
2023-02-16 06:18:16 -0800

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints +, 0 or - depending on n
+ * @c: character to be used
+ * Return: int 0, 1 or -1
+*/
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(n + '0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
