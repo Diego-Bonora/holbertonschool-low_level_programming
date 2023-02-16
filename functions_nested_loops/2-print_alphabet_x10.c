b85db06f48f940912a7b7a7dbfcc042520e6cd4c
Diego Bonora <bonoradi@gmail.com>
2023-02-16 05:45:23 -0800

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return: nothing
+*/
+void print_alphabet_x10(void)
+{
+	int number;
+	int letter;
+
+	for (number = 0; number <= 10; number++)
+	}
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
