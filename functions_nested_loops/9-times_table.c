a1b6ee6998cd864b476be84461fa77f4c46e2a56
Diego Bonora <bonoradi@gmail.com>
2023-02-17 04:51:07 -0800

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - prints the tables from 0 to 9
+ * Return: nothing
+*/
+void times_table(void)
+{
+	int number;
+	int multiplication;
+
+	for (number = 0; number <= 9; number++)
+	{
+		for (multiplication = 0; multiplication <= 9; multiplication++)
+		{
+			_putchar(number * multiplication + '0');
+			if (multiplication != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
