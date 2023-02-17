67b0da185e3a2a3244cf08d97adce7dcce3df59f
Diego Bonora <bonoradi@gmail.com>
2023-02-17 09:00:52 -0800

    100-times_table.c

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table - prints the tables from 0 to 9
+ * @n: input number
+ * Return: nothing
+*/
+void print_times_table(int n)
+{
+	int number;
+	int multiplication;
+	int multi_number;
+
+	if (n < 0 || n > 15)
+	{
+
+	}
+	else
+	{
+		for (number = 0; number <= n; number++)
+		{
+			for (multiplication = 0; multiplication <= 9; multiplication++)
+			{
+				multi_number = number * multiplication;
+				if (multi_number >= 10)
+				{
+					_putchar(multi_number / 10 + '0');
+					_putchar(multi_number % 10 + '0');
+				}
+				else
+				{
+					if (multiplication != 0)
+					{
+						_putchar(' ');
+					}
+					_putchar(multi_number + '0');
+				}
+					if (multiplication != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
