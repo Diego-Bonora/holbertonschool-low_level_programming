58231b4d83af7a7f65565713f47d94d3940320bd
Diego Bonora <bonoradi@gmail.com>
2023-02-17 04:58:11 -0800

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,13 +7,23 @@ void times_table(void)
 {
 	int number;
 	int multiplication;
+	int multi_number;
 
 	for (number = 0; number <= 9; number++)
 	{
 		for (multiplication = 0; multiplication <= 9; multiplication++)
 		{
-			_putchar((number * multiplication) + '0');
-			if (multiplication != 9)
+			multi_number = number * multiplication;
+			if (multi_number >= 10)
+			{
+				_putchar(multi_number / 10 + '0');
+				_putchar(multi_number % 10 + '0');
+			}
+			else
+			{
+				_putchar(multi_number + '0');
+			}
+				if (multiplication != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
