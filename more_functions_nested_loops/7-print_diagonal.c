f6669522c4f249fc1eea33fbdf622891b68e8eea
Diego Bonora <bonoradi@gmail.com>
2023-02-22 09:49:03 -0800

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_diagonal - prints \ diagonaly n times
+ * @n: input number
+ * Return:nothing
+*/
+
+void print_diagonal(int n)
+{
+	int number;
+	int spaces;
+
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (number = 0; number < n; number++)
+		{
+			spaces = number;
+			while (spaces != 0)
+			{
+				_putchar(' ');
+				spaces--;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
