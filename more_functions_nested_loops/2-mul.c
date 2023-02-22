b6a248991f7a466decb221eb9cb36a645e067708
Diego Bonora <bonoradi@gmail.com>
2023-02-22 09:24:08 -0800

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.c"
+/**
+ * mul - multiplies two integers
+ * @a:input number 1
+ * @b:input number 2
+ * Return:multiplication of 2 numbers
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
