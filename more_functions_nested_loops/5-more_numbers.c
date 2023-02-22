61c4b338646a885aacb8815ce7834d411c6b67c1
Diego Bonora <bonoradi@gmail.com>
2023-02-22 09:38:55 -0800

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times numbers from 0 to 14
+ * Return:nothing
+*/
+
+void more_numbers(void)
+{
+	int times;
+	int number;
+
+	for (times = 0; times <= 9; times++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			if (number >= 10)
+			{
+				_putchar(number / 10 + '0');
+			}
+			_putchar(number % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
