3228c45664587a326528f58e83b2c412a33f2790
Diego Bonora <bonoradi@gmail.com>
2023-02-23 07:59:45 -0800

    101-print_number.c

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,12 +9,18 @@ void print_number(int n)
 {
 	int left;
 	int right;
+	int signo = 1;
+
+	if (n < 0)
+	{
+		signo = -1;
+	}
 
 	if (n >= 1000)
 	{
 		right = n / 100;
 		left = n % 100;
-		_putchar(right / 10 + '0');
+		_putchar((right / 10) * signo + '0');
 		_putchar(right % 10 + '0');
 		_putchar(left / 10 + '0');
 		_putchar(left % 10 + '0');
@@ -23,13 +29,13 @@ void print_number(int n)
 	{
 		right = n / 10;
 		left = n % 10;
-		_putchar(right / 10 + '0');
+		_putchar((right / 10) * signo + '0');
 		_putchar(right % 10 + '0');
 		_putchar(left);
 	}
 	else if (n >= 10)
 	{
-		_putchar(n / 10 + '0');
+		_putchar((n / 10) * signo + '0');
 		_putchar(n % 10 + '0');
 	}
 	else
