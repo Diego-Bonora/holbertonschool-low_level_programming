23a297dd652d28eac74b13866c0b43c711b56bd9
Diego Bonora <bonoradi@gmail.com>
2023-02-24 11:29:47 -0800

    101-print_number.c

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_number - prtins a number using _putchar
  * @n: input number
@@ -7,7 +8,33 @@
 
 void print_number(int n)
 {
-	unsigned int num;
-	num = n;
+	int num = 0;
+	int negativo = 0;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		negativo = 1;
+	}
+	while (n >= 10)
+	{
+		num = num * 10;
+		num = num + n % 10;
+		n = n / 10;
+	}
+	num = num * 10;
+	num = num + n % 10;
+
+	if (negativo == 1)
+	{
+		_putchar('-');
+	}
+
+	while (num >= 10)
+	{
+		_putchar(num % 10 + '0');
+		num = num / 10;
+	}
 	_putchar(num + '0');
+
 }
