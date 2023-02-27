eb005fb0c35be3208739d87119b359b374fa96c6
Diego Bonora <bonoradi@gmail.com>
2023-02-27 10:18:39 -0800

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: input character
+ * Return:nothing
+*/
+
+void puts2(char *str)
+{
+	int len;
+	int count;
+
+	for (len = 0; str[len]; len++)
+	{}
+
+	for (count = 0; count <= len; count++)
+	{
+		if (count % 2 == 0)
+			_putchar(str[count]);
+	}
+	_putchar('\n');
+}
