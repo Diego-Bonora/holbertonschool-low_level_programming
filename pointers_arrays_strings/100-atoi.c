386827b118ce8936636e1b05074ffc42113f2419
Diego Bonora <bonoradi@gmail.com>
2023-02-28 09:37:59 -0800

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,49 +1,42 @@
 #include "main.h"
 /**
- * main -
- * Return:
+ * _atoi - atoi
+ * @s: string input
+ * Return: numbers
 */
 
 int _atoi(char *s)
 {
-	long int num = 0;
+	int num = 0;
 	int sign = 1;
 	int len = 0;
 	int lens;
 	int top = 0;
 
 	for (lens = 0; s[lens]; lens++)
-	{}
-
+	{
+		if (s[len] == '+')
+			sign *= 1;
+		else if (s[len] == '-')
+			sign *= -1;
+	}
 	while (top != 1)
 	{
 		if (s[len] >= 48 && s[len] <= 57)
 		{
 			num = num * 10;
 			num = num + s[len] - '0';
+			if (top != 2)
+				num *= sign;
 			top = 2;
 		}
-		
 		if (top == 2 && (s[len] < 48 && s[len > 57]))
-		{
 			top = 1;
-		}
 
-		if (s[len] == '+')
-		{
-			sign *= 1;
-		}
-		else if (s[len] == '-')
-		{
-			sign *= -1;
-		}
 		if (len == lens)
-		{
 			top = 1;
-		}
 		len++;
 	}
-	num *= sign;
 	return (num);
 }
 
