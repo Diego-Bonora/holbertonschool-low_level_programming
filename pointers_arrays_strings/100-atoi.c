76b50525c9fb4f714fe0301f9384bac9d012d8af
Diego Bonora <bonoradi@gmail.com>
2023-02-28 09:27:53 -0800

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,11 @@ int _atoi(char *s)
 	int num = 0;
 	int sign = 1;
 	int len = 0;
+	int lens;
 	int top = 0;
+
+	for (lens = 0; s[lens]; lens++)
+	{}
 
 	while (top != 1)
 	{
@@ -32,6 +36,10 @@ int _atoi(char *s)
 		else if (s[len] == '-')
 		{
 			sign *= -1;
+		}
+		if (len == lens)
+		{
+			top = 1;
 		}
 		len++;
 	}
