1d401adf466585fd61c6f2581668d6e969a55891
Diego Bonora <bonoradi@gmail.com>
2023-02-28 04:51:41 -0800

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copies a string into another string
+ * @dest: output string
+ * @src: input string
+ * Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+
+	for (len = 0; src[len]; len++)
+	{
+		dest[len] = src[len];
+	}
+	return (dest);
+}
