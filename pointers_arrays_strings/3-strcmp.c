a469e0f03087dd3c07cf5dd34f5f722d7ffdc35b
Diego Bonora <bonoradi@gmail.com>
2023-03-01 06:49:03 -0800

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,23 @@
 #include "main.h"
 /**
- * main -
- * Return:
+ * _strcmp - compares 2 strings
+ * @s1: output string
+ * @s2:	input string
+ * Return: int
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	
+	int len;
+	int num = 0;
+
+	for (len = 0; s1[len] && s2[len]; len++)
+	{
+		if (s1[len] != s2[len])
+		{
+			num = s1[len] - s2[len];
+			break;
+		}
+	}
+	return (num);
 }
