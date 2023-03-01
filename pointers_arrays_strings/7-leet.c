421bb42a92dbd5dcc26e922408080fc82fb210d8
Diego Bonora <bonoradi@gmail.com>
2023-03-01 09:53:15 -0800

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet -
+ * @n
+ * Return:
+*/
+
+char *leet(char *n)
+{
+	int listascii[] = {65, 97, 69, 101, 79, 111, 84, 116, 76, 108}
+	int listanum[] = {52, 52, 51, 51, 48, 48, 55, 55, 49, 49}
+	int len;
+	int len2;
+
+	for (len = 0; listascii[len]; len++)
+	{
+		for (len2 = 0; n[len2]; len2++)
+		{
+			if (n[len2] == listaascii[len])
+			{
+				n[len2] = listanum[len];
+			}
+		}
+	}
+	return (n);
+}
