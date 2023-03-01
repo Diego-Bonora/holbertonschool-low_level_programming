ab7e74ceba538d70c3add129aad6ff69a2f2b81d
Diego Bonora <bonoradi@gmail.com>
2023-03-01 05:03:05 -0800

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: output string
+ * @src: input string
+ * Return: string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int len;
+	int lent;
+
+	for (len = 0; dest[len]; len++)
+	{}
+
+	for (lent = 0; src[lent]; lent++)
+	{
+		dest[len] = src[lent];
+		len++;
+	}
+	return (*dest)
+}
