49805c71e47917e740b41229652905df661c2982
Diego Bonora <bonoradi@gmail.com>
2023-03-01 05:35:24 -0800

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _strncpy - copies a string into another string
+ * @dest: string output
+ * @src: string input
+ * @n: input number
+ * Return: string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len;
+
+	for (len = 0; len <= n; len++)
+	{
+		dest[len] = src[len];
+	}
+	return (dest);
+}
