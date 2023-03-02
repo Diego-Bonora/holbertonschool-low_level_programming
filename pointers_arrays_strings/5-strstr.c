5bfca9d16c8b3cc7c7ae164b9bea97a6b4679d96
Diego Bonora <bonoradi@gmail.com>
2023-03-02 14:02:59 -0800

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -6,5 +6,31 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	
+	int len;
+	int largo;
+	int count = 0;
+	int stop = 0;
+
+	(void)len;
+	for (largo = 0; needle[largo]; largo++)
+	{}
+
+	while (stop == 0)
+	{
+		if (needle[count] == *haystack)
+		{
+			count++;
+		}
+		else
+		{
+			count = 0;
+		}
+		if (count == largo)
+		{
+			stop = 1;
+			haystack -= largo;
+		}
+		haystack++;
+	}
+	return (haystack);
 }
