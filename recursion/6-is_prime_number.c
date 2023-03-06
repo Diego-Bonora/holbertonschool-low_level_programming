4e944d643159fe2fec269ad5572fcd36ff306302
Diego Bonora <bonoradi@gmail.com>
2023-03-06 09:25:23 -0800

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,34 @@
 #include "main.h"
 /**
- * main -
- * Return:
+ * is_prime_number - returns 1 if n its a prime number
+ * @n: input number
+ * Return: 1 or 0
 */
 
 int is_prime_number(int n)
 {
-	
+	int num = n;
+
+	return (numsum(num, n));
+}
+
+/**
+ * numsum - function for finding prime number
+ * @x: input auxiliar number
+ * @n: input number
+ * Return: 1 or 0
+ */
+
+int numsum(int x, int n)
+{
+	num--;
+	if (n % num == 0 && num != 0)
+	{
+		return (0);
+	}
+	else if (num == 0)
+	{
+		return (1);
+	}
+	return (numsum(num, n));
 }
