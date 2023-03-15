a104dfa29d28bebc068b25bd3b2eea9bce33dd58
Diego Bonora <bonoradi@gmail.com>
2023-03-15 07:27:51 -0700

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates mem using malloc
+ * @b: input unsigned int
+ * Return: nothing
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+}
