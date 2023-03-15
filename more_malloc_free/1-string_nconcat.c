606a3047440594c910cd9ccfc21a2e8fe5dc1a93
Diego Bonora <bonoradi@gmail.com>
2023-03-15 10:17:50 -0700

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			len2 = n;
 		}
 	}
-	newstr = malloc((len + len2) * sizeof(char));
+	newstr = malloc((len + len2) * sizeof(char) + 1);
 	if (!newstr)
 	{
 		return(NULL);
