3173ababab178a76a85f56f0666e01f33ff5a610
Diego Bonora <bonoradi@gmail.com>
2023-03-15 13:30:59 -0700

    betty_fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,49 +18,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	(void)newstrlen;
 	(void)len3;
 	if (s1 == NULL)
-	{
 		len = 0;
-	}
 	else
 	{
 		for (len = 0; s1[len]; len++)
-		{}
-	}
-
+		{} }
 	if (s2 == NULL)
-	{
 		len2 = 0;
-	}
 	else
 	{
 		for (len2 = 0; s2[len2]; len2++)
 		{}
 		if (len2 > n)
-		{
-			len2 = n;
-		}
-	}
+			len2 = n; }
 	newstr = malloc((len + len2) * sizeof(char) + 1);
 	if (!newstr)
-	{
-		return(NULL);
-	}
+		return (NULL);
 	if (len != 0)
 	{
 		for (newstrlen = 0; newstrlen <= len; newstrlen++)
 		{
-			newstr[newstrlen] = s1[newstrlen];
-		}
-		newstrlen--;
-	}
+			newstr[newstrlen] = s1[newstrlen]; }
+		newstrlen--; }
 	if (len2 != 0)
 	{
-		for (len3 = 0; len3 <= len2; len3++)
+		for (len3 = 0; len3 < len2; len3++)
 		{
 			newstr[newstrlen] = s2[len3];
-			newstrlen++;
-		}
-	}
+			newstrlen++; }}
 	newstr[newstrlen] = '\0';
 	return (newstr);
 }
