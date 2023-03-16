b7fe8b3bb245ab387a9ee9aca7469194f682bad6
Diego Bonora <bonoradi@gmail.com>
2023-03-16 05:11:18 -0700

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -6,13 +6,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *pointer = NULL;
+	void *pointer;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	pointer = malloc(nmemb * sizeof(size));
+	pointer = malloc(nmemb * size);
 	if (!pointer)
 	{
 		return (NULL);
