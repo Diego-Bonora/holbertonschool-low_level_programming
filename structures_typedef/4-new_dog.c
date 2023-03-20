fe9a64927f4dd0223e94b0656652943d6d92fda7
Diego Bonora <bonoradi@gmail.com>
2023-03-20 10:02:58 -0700

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,7 +12,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *newdog = NULL;
 	newdog = malloc(sizeof(dog_t));
 	if (!newdog)
+	{
 		return (NULL);
+	}
 	if (!name || !age || !owner)
 	{
 		return (NULL);
@@ -20,15 +22,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	newdog->name = malloc(sizeof(char) * strlen(name));
 	if (!newdog->name)
+	{
 		free(newdog);
 		return (NULL);
+	}
 	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = malloc(sizeof(char) * strlen(owner));
 	if (!newdog->owner)
+	{
 		free(newdog->name);
 		free(newdog);
 		return (NULL);
+	}
 	newdog->owner = owner;
 	return (newdog);
 }
