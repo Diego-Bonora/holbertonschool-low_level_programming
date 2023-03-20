a0aa4205f506a445c2e59283af309f6d5bb801ab
Diego Bonora <bonoradi@gmail.com>
2023-03-20 06:52:04 -0700

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialices the structure dog
+ * @d: structure name
+ * @name: name for the dog
+ * @age: age for the dog
+ * @owner: owner for the dog
+ * Return: nothing
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
