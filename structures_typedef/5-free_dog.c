68cfc8a6f9b3f379a6e6b7faf101f08e5189829e
Diego Bonora <bonoradi@gmail.com>
2023-03-20 10:24:01 -0700

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - free the memory of dog
+ * @d: input dog
+ * Return: nothing
+*/
+
+void free_dog(dog_t *d)
+{
+	free((*d).nombre);
+	free((*d).age);
+	free((*d).owner);
+	free(d);
+}
