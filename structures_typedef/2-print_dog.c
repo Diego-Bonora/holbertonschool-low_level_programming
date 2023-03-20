7b8331ccdbb1f313986bc0c990cfedbb68fcd03e
Diego Bonora <bonoradi@gmail.com>
2023-03-20 07:39:55 -0700

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints the dog structure
+ * @d: structure input
+ * Return: nothing
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!(*d).name)
+			(*d).name = "(nill)";
+		if (!(*d).owner)
+			(*d).owner = "(nill)";
+		if (!(*d).age)
+		{
+			printf("Name: %s\nAge: (nill)\nOwner: %s\n", d->name, d->owner);
+		}
+		else
+		{
+			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		}
+	}
+}
