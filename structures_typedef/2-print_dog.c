2a0fd593e3e6b3baede1470a47359f68781af9f9
Diego Bonora <bonoradi@gmail.com>
2023-03-20 07:45:28 -0700

    syntax_fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,11 +11,11 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (!(*d).name)
-			(*d).name = "(nill)";
+			(*d).name = "(nil)";
 		if (!(*d).age)
 			(*d).age = 0;
 		if (!(*d).owner)
-			(*d).owner = "(nill)";
+			(*d).owner = "(nil)";
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
