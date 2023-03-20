d89366f7ce8ac0dfc596e9587409b2cb782d668d
Diego Bonora <bonoradi@gmail.com>
2023-03-20 06:33:17 -0700

    betty_fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - creates a dog structure
+ * @name: name for the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: creates a dog structure with name, age and owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
