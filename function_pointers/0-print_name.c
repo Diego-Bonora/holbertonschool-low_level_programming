d1125a8b2767a71522461d15fdd24aa1b4f0d0ac
Diego Bonora <bonoradi@gmail.com>
2023-03-21 05:15:14 -0700

    betty_fix

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name -
- * @name:
- * @f:
- * Return:
+ * print_name - prints a name
+ * @name: input name
+ * @f: pointer to function
+ * Return: nothing
 */
 
 void print_name(char *name, void (*f)(char *))
-{	
+{
 	if (f)
 	{
 		f(name);
