325a8a8a3198f05cd9f7738cdb43d49a3a881c4b
Diego Bonora <bonoradi@gmail.com>
2023-03-22 07:27:15 -0700

    2-print_strings.c

---
+++ variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 3, "Jay", "algo", NULL);
+    return (0);
+}
---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints n strings followed by a separator
+ * @separator: the separator to be printed between strings
+ * @n: ammount of strings
+ * Return:
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int len;
+	char *temp;
+	va_list ap;
+
+	va_start(ap, n);
+	for (len = 0; len < (n - 1); len++)
+	{
+		temp = va_arg(ap, char*);
+		if (!separator)
+			if (!temp)
+				printf("(nil)");
+			else
+				printf("%s", temp);
+		else
+			if (!temp)
+				printf("(nil)%s", separator);
+			else
+				printf("%s%s", temp, separator);
+	}
+	temp = va_arg(ap, char*);
+	if (temp == NULL)
+		printf("(nil)");
+	else
+	{
+		printf("%s", temp);
+	}
+	putchar('\n');
+	va_end(ap);
+}
