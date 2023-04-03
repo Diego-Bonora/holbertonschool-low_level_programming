31bef2ff63a131c5b54a0acfb74534dda8e7c963
Diego Bonora <bonoradi@gmail.com>
2023-04-03 11:38:12 -0700

    betty fix

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,14 +1,15 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ * free_list - frees a linked list
+ * @head: input linked list
+ * Return: Nothing
  */
 
 void free_list(list_t *head)
 {
 	list_t *temp;
+
 	temp = head;
 	while (head != NULL)
 	{
