eb25ac4ed70284aa9a871acc0437684f61ba0d46
Diego Bonora <bonoradi@gmail.com>
2023-04-03 07:06:18 -0700

    function for printing a linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * print_list - prints a linked list
+ * @h: input node
+ * Return: amount of lists printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	list_t *temp;
+	int lenght = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+	{
+		return (-1);
+	}
+
+	*temp = *h;
+
+	while(temp != NULL)
+	{
+		if (temp->str)
+		{	
+			printf("[%u] %s\n", temp->len, temp->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		lenght++;
+		temp = temp->next;
+	}
+
+	free(temp);
+	return (lenght);
+}
