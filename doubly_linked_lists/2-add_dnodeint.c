051a39a82dadc5ce0ed1709f6a572c2366fd5880
Diego Bonora <bonoradi@gmail.com>
2023-04-04 06:29:25 -0700

    small backward fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!temp)
 		return (NULL);
 
-	if (!head)
+	if (!*head)
 	{
 		temp->n = n;
 		temp->prev = NULL;
@@ -27,6 +27,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		temp->n = n;
 		temp->prev = NULL;
 		temp->next = *head;
+		(*head)->prev = temp;
 		*head = temp;
 	}
 	return (*head);
