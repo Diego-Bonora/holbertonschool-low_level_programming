9565b15f41e3c2403d486e29c11bfeeade89d130
Diego Bonora <bonoradi@gmail.com>
2023-04-04 10:18:55 -0700

    function to sum all the numbers from a linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums all numbers from a linked list
+ * @head: input node
+ * Return: returns the sum of all the numbers
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int num = 0;
+
+	if (!head)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
