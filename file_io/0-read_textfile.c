1660ffad8a2bccf3466405a4cb790ae263825f9e
Diego Bonora <bonoradi@gmail.com>
2023-04-11 07:27:07 -0700

    file for printing text from a file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads and prints text from a file
+ * @filename: the name of the file to read
+ * @letters: amount of chars to be printed
+ * Return: returns the amount of chars printed
+ */
+
+size_t read_textfile(const char *filename, size_t letters)
+{
+	char letter;
+	size_t count = 0;
+	FILE *ptr;
+
+	if (!filename)
+		return (0);
+
+	ptr = fopen(filename, "r");
+	if (!ptr)
+		return (0);
+
+	letter = fgetc(ptr);
+	while (letters != 0 && letter != EOF)
+	{
+		write(1, &letter, 1);
+		letter = fgetc(ptr);
+		count++;
+		letters--;
+	}
+	fclose(ptr);
+	return (count++);
+}
