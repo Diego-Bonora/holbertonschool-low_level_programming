20d45c0dfa9796d9deea1be9be66628bfece6ce9
Diego Bonora <bonoradi@gmail.com>
2023-04-12 15:41:04 -0700

    a lot of fixes made

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,66 +9,42 @@
 
 int main(int argc, char *argv[])
 {
-	int ptr;
+	int ptr, true = 1;
 	FILE *ptr2;
 	char buff[1024];
-	int flag = 0;
 	int check = 0;
 
 	if (argc != 3)
-	{
-		_return(1, NULL);
-	}
+	{	write(2, "Usage: cp file_from file_to\n", 28);
+		exit(97); }
 	ptr = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	ptr2 = fopen(argv[1], "r");
 	if (!ptr2)
-	{
-		_return(2, argv[1]);
-	}
+	{	write(STDERR_FILENO, "Error: Can't read from file ", 28);
+		write(STDERR_FILENO, argv[1], strlen(argv[1]));
+		exit(98); }
 	if (ptr == -1)
-	{
-		_return(3, argv[2]);
-	}
+	{	write(STDERR_FILENO, "Error: Can't write to ", 22);
+		write(STDERR_FILENO, argv[2], strlen(argv[2]));
+		exit(99); }
 	memset(buff, 0, sizeof(buff));
 	fgets(buff, 1024, ptr2);
-	while (flag == 0)
+	while (true)
 	{
-		check = write(ptr, buff, 1024);
+		check = write(ptr, buff, strlen(buff));
 		if (check == -1)
-			_return(3, argv[2]);
+		{	write(STDERR_FILENO, "Error: Can't write to ", 22);
+			write(STDERR_FILENO, argv[2], strlen(argv[2]));
+			exit(99); }
 		if (feof(ptr2))
-			flag = 1;
-		else
-		{
-			memset(buff, 0, sizeof(buff));
-			fgets(buff, 1024, ptr2);
-		}
+			break;
+		memset(buff, 0, sizeof(buff));
+		fgets(buff, 1024, ptr2);
 	}
 	close(ptr);
 	fclose(ptr2);
+	if (!ptr || !ptr2)
+	{	write(STDERR_FILENO, "Error: Can't close fd ", 22);
+		exit(100); }
 	return (0);
-}
-
-void _return(int section, char *argument)
-{
-	(void)argument;
-	if (section == 1)
-	{
-		write(2, "Usage: cp file_from file_to\n", 28);
-		exit(97);
-	}
-	else if (section == 2)
-	{
-		write(2, "Error: Can't read from file ", 27);
-		write(2, argument, strlen(argument));
-		write(2, "\n", 1);
-		exit(98);
-	}
-	else if (section == 3)
-	{
-		write(2, "Error: Can't read from file ", 27);
-		write(2, argument, strlen(argument));
-		write(2, "\n", 1);
-		exit(98);
-	}
 }
