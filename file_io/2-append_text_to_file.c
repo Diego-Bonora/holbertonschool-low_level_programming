0552a51fa4c06cba39bef4adf2d7816d2f2365d7
Diego Bonora <bonoradi@gmail.com>
2023-04-12 05:33:57 -0700

    permission check

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,6 +16,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	ptr = fopen(filename, "a");
+	if (ptr == -1)
+		return (-1);
 	fprintf(ptr, "%s", text_content);
 	fclose(ptr);
 	return (1);
