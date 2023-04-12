cc812019af78005502ed4a24c145d55d8fb7c75e
Diego Bonora <bonoradi@gmail.com>
2023-04-12 15:03:24 -0700

    test

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,33 +13,29 @@ int main(int argc, char *argv[])
 	FILE *ptr2;
 	char buff[1024];
 	int flag = 0;
+	int check = 0;
 
 	if (argc != 3)
 	{
-		write(2, "Usage: cp file_from file_to\n", 28);
-		exit(97);
+		_return(1, NULL);
 	}
 	ptr = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	ptr2 = fopen(argv[1], "r");
 	if (!ptr2)
 	{
-		write(2, "Error: Can't read from file", 27);
-		write(2, argv[1], strlen(argv[1]));
-		write(2, "\n", 1);
-		exit(98);
+		_return(2, argv[1]);
 	}
 	if (ptr == -1)
 	{
-		write(2, "Error: Can't write to", 21);
-		write(2, argv[2], strlen(argv[2]));
-		write(2, "\n", 1);
-		exit(99);
+		_return(3, argv[2]);
 	}
 	memset(buff, 0, sizeof(buff));
 	fgets(buff, 1024, ptr2);
 	while (flag == 0)
 	{
-		write(ptr, buff, strlen(buff));
+		check = write(ptr, buff, 1024);
+		if (check == -1)
+			_return(3, argv[2]);
 		if (feof(ptr2))
 			flag = 1;
 		else
@@ -51,4 +47,28 @@ int main(int argc, char *argv[])
 	close(ptr);
 	fclose(ptr2);
 	return (0);
+}
+
+void _return(int section, char *argument)
+{
+	(void)argument;
+	if (section == 1)
+	{
+		write(2, "Usage: cp file_from file_to\n", 28);
+		exit(97);
+	}
+	else if (section == 2)
+	{
+		write(2, "Error: Can't read from file ", 27);
+		write(2, argument, strlen(argument));
+		write(2, "\n", 1);
+		exit(98);
+	}
+	else if (section == 3)
+	{
+		write(2, "Error: Can't read from file ", 27);
+		write(2, argument, strlen(argument));
+		write(2, "\n", 1);
+		exit(98);
+	}
 }
