68c4f81052aedb7b3de920a9e411c1525703496e
Diego Bonora <bonoradi@gmail.com>
2023-04-13 09:54:58 -0700

    memory loss fix

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -16,16 +16,16 @@ void hash_table_delete(hash_table_t *ht)
 	for (count = 0; count < ht->size; count++)
 	{
 		temp = ht->array[count];
+		delete = temp;
 		while (temp)
 		{
-			delete = temp;
 			temp = temp->next;
 			free(delete->key);
 			free(delete->value);
 			free(delete);
+			delete = temp;
 		}
 	}
 	free(ht->array);
 	free(ht);
-
 }
