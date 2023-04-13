1d1ea6a90026b23cc23decafbdeedfcfa8b3f511
Diego Bonora <bonoradi@gmail.com>
2023-04-13 09:01:43 -0700

    function for printing the hole hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the hash table
+ * @ht: hash table input
+ * Return: nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int count;
+	hash_node_t *temp;
+	char *space = "";
+
+	if (ht == NULL)
+		return;
+
+	for (count = 0; count < ht->size; count++)
+	{
+		temp = ht->array[count];
+		while (temp)
+		{
+			printf("%s", space);
+			printf("'%s': '%s'", temp->key, temp->value);
+			space = ", ";
+			temp = temp->next;
+		}
+	}
+	putchar('\n');
+}
